8fee4e5a4dabaa9be6afe72c0cf034a680bb0e82
Codenade <amadeus.dorian04@gmail.com>
2023-10-11 15:52:33 +0200

    unity assets build now writes https://github.com/game-ci/documentation/blob/57600a71e0d046cf8efa00662db86ace7a8450d8/example/BuildScript.cs#L182-L196 to the log because else game-ci/builder  fails: https://github.com/game-ci/unity-builder/blob/a13443a7463b96cfcf3d0403bef3f7fae4b30b55/src/model/exec-with-error-check.ts#L16-L26

--- ksp2-papi-assets/Assets/Editor/BuildAssets.cs
+++ ksp2-papi-assets/Assets/Editor/BuildAssets.cs
@@ -1,14 +1,35 @@
 #if UNITY_EDITOR
 
+using System;
 using UnityEditor;
+using UnityEditor.Build.Reporting;
 
 public static class BuildAssets
 {
+    private static readonly string Eol = Environment.NewLine;
+
     [MenuItem("Build/Addressables")]
     public static void PerformBuild()
     {
         UnityEditor.AddressableAssets.Settings.AddressableAssetSettings.CleanPlayerContent();
         UnityEditor.AddressableAssets.Settings.AddressableAssetSettings.BuildPlayerContent();
+        ReportSummary(new BuildSummary());
+    }
+
+    private static void ReportSummary(BuildSummary summary)
+    {
+        Console.WriteLine(
+            $"{Eol}" +
+            $"###########################{Eol}" +
+            $"#      Build results      #{Eol}" +
+            $"###########################{Eol}" +
+            $"{Eol}" +
+            $"Duration: {summary.totalTime}{Eol}" +
+            $"Warnings: {summary.totalWarnings}{Eol}" +
+            $"Errors: {summary.totalErrors}{Eol}" +
+            $"Size: {summary.totalSize} bytes{Eol}" +
+            $"{Eol}"
+        );
     }
 }
 
